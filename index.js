0bc929f3fcecf3a7bf7f600edf81f69c33a7f2f4
Matheus Vianna Matos <101771378+matheusvianna7@users.noreply.github.com>
2022-06-26 18:24:09 -0700

    Add files for Scrimba project

---
+++ index.js
@@ -0,0 +1,95 @@
+let player1Score = 0
+let player2Score = 0
+let player1Turn 
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+const chooseBtn = document.getElementById("chooseBtn")
+const container = document.getElementById("container")
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+function choosePlayer(){
+    const randomNumber = Math.floor(Math.random() * 2) + 1
+    
+    if (randomNumber === 1){
+        player1Turn = 1
+        message.textContent = "Player 1 Turn"
+        player1Dice.classList.add("active")
+        player2Dice.classList.remove("active")
+    } else {
+        player1Turn = 2
+        message.textContent = "Player 2 Turn"
+        player2Dice.classList.add("active")
+        player1Dice.classList.remove("active")
+    }
+    
+    chooseBtn.style.display = "none"
+}
+
+chooseBtn.addEventListener("click", function(){
+    choosePlayer()
+})
+
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn === 1) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player2Dice.textContent = "-"
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+        player1Turn = 2
+    } else if(player1Turn === 2) {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player1Dice.textContent = "-"
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+        player1Turn = 1
+    }
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+        container.style.animation = "winner 1.5s infinite"
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+        container.style.animation = "winner 1.5s infinite"
+    }
+})
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player Turn"
+    resetBtn.style.display = "none"
+    chooseBtn.style.display = "block"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.remove("active")
+    container.style.animation = "none"
+}
